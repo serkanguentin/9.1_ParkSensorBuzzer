ba50dd6f517a0cecaa19498bfa66a93746c528b3
serkanguentin <guntinfurkan@gmail.com>
2024-05-29 10:49:34 +0200

    ParkSensorBuzzerr

--- 9_ParkSensorBuzzer.ino
+++ 9_ParkSensorBuzzer.ino
@@ -1,50 +1,50 @@
-                           //LCD kütüphanemizi başlatıyoruz.
-int trigPin = 10;// 7;                                        //Ultrasonik sensör trig pini değişkeni
+                          
+int trigPin = 10;// 7;     
 int echoPin = 13;// 6;    
-int buzzerPin= 3;// 8;                                    //Ultrasonik sensör echo pini değişkeni
-int sure;                                               //Ses dalgasının gidip gelme süresi değişkeni
-int uzaklik ;
-int uyariLed ;                                            //Ölçülen uzaklık değeri değişkeni
-            //LCD'nin pin bağlantılarını ayarlıyoruz.
+int buzzerPin= 3;// 8      
+int time;                  
+int distance ;
+int uyariLed ;                                           
+            
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);                             //trig pinini OUTPUT olarak ayarlıyoruz.
+  pinMode(trigPin, OUTPUT);                           
   pinMode(echoPin,INPUT); 
-  pinMode(buzzerPin, OUTPUT);                                //echo pinini INPUT olarak ayarlıyoruz.
-                                  //LCD ekranımızın en-boy oranını ayarlıyoruz.                               
+  pinMode(buzzerPin, OUTPUT);       
 }
 void loop() {
-  digitalWrite(trigPin, LOW);                           //Ultrasonik sensör ile ölçüm sekansını başlatıyoruz.
+  digitalWrite(trigPin, LOW);                           
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(100);
   digitalWrite(trigPin, LOW);
-  sure = pulseIn(echoPin, HIGH, 11600);                 //Ses dalgasının gidip gelme süresini ölçüyoruz.
-  uzaklik= sure*0.0345/2;       
-  Serial.println(uzaklik);
-                          //Ölçülen süre ile uzaklık hesabı yapıyoruz.
+  time = pulseIn(echoPin, HIGH, 11600);                 
+  distance= time*0.0345/2;       
+  Serial.println(distance);
+                          
  
- if(uzaklik>0 && uzaklik<=15) //Mesafe 10 cm den uzun 20cm de eşit veya kısaysa aşağdaki işlemler gerçekleşir.
+ if(distance>0 && distance<=15) 
 {
 digitalWrite(uyariLed , HIGH);
 digitalWrite(buzzerPin, HIGH);
-delay(150); // ledin yanık kalma süresiyle buzzerin uyarı süresi standart bir süreye 150ms ye ayarlandı.
+delay(150); 
+
 digitalWrite(uyariLed , LOW);
 digitalWrite(buzzerPin , LOW);
 delay(100);
 }
 
- else if(uzaklik>15 && uzaklik<=30) //Mesafe 10 cm den uzun 20cm de eşit veya kısaysa aşağdaki işlemler gerçekleşir.
+ else if(distance>15 && distance<=30) 
 {
 digitalWrite(uyariLed , HIGH);
 digitalWrite(buzzerPin, HIGH);
-delay(150); // ledin yanık kalma süresiyle buzzerin uyarı süresi standart bir süreye 150ms ye ayarlandı.
+delay(150);
 digitalWrite(uyariLed , LOW);
 digitalWrite(buzzerPin , LOW);
 delay(200);
 }
-else if(uzaklik>30 && uzaklik<=45) //Mesafe 20 cm den uzun 30cm de eşit veya kısaysa aşağdaki işlemler gerçekleşir.
+else if(distance>30 && distance<=45) 
 {
 digitalWrite(uyariLed , HIGH);
 digitalWrite(buzzerPin , HIGH);
@@ -54,7 +54,7 @@ digitalWrite(buzzerPin , LOW);
 delay(400);
 }
 
-else if(uzaklik>45 && uzaklik<=60) //Mesafe 40 cm den uzun 50cm de eşit veya kısaysa aşağdaki işlemler gerçekleşir.
+else if(distance>45 && distance<=60) 
 {
 digitalWrite(uyariLed , HIGH);
 digitalWrite(buzzerPin , HIGH);
